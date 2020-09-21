1a6be6a008a73ede491fc7ef7c49290b489db606
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-09-21 22:29:01 +0900

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,4 +1,3 @@
 let dom = document.querySelector('#root');
-      let element = React.createElement( 
-        'p', {}, 'Hello React Application!');
+let element = React.createElement( 'p', {}, 'Hello React Application!');
       ReactDOM.render(element, dom);
