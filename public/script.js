d8001c7d1fe82c0cd57d29520670121bad616c95
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-09-22 23:49:29 +0900

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,9 +1,12 @@
 let dom = document.querySelector('#root');
 
 let element = React.createElement( 
-  'div', {}, '[
+  'div', {}, [
     React.createElement(
-      
+      'h2', {}, "Hello!"
+    ),
+    React.createElement(
+      'h3', {}
     )
   ]);
       ReactDOM.render(element, dom);
