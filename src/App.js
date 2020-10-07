d02d6d63f2e26f50fff22ad389112ecfa5d01d95
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-10-07 22:53:32 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super();
+    this.title = props.title;
+    this.message = props.message;
+  }
   render() {
     return <div>
-      <h1>React</h1>
-      <p>This is sample component.</p>
-      <p>これはサンプルのコンポーネントです。</p>
+      <h1>{this.title}</h1>
+      <p>{this.message}</p>
     </div>;
   }
 }
