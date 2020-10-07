220a2fc76c44af973b13f782cdfa6d7a4ea3abca
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-10-07 22:43:10 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
+class App extends Component {
+  render() {
+    return <div>
+      <h1>React</h1>
+      <p>This is sample component</p>
+      <p>これはサンプルのコンポーネントです。</p>
+    </div>;
+  }
+}
 
 export default App;
