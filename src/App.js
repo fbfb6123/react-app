19f60b9aa43bff2af98253d8e8b4fbe9ac4c88c1
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-10-10 23:16:16 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import './App.css';
 class App extends Component {
   render() {
     return <div>
-      <h1>{this.title}</h1>
-      <p>{this.message}</p>
+      <h1>React</h1>
+      <Rect x="50" y="50" w="150" h="150" c="cyan" />
+      <Rect x="150" y="100" w="150" h="150" c="magenta" />
+      <Rect x="100" y="150" w="150" h="150" c="black" />
     </div>;
   }
 }
