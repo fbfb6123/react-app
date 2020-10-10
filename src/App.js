fdda876fee8a4b14a6f42b9c9cdbf25e71e4086b
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-10-10 23:13:33 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-
-  constructor(props) {
-    super();
-    this.title = props.title;
-    this.message = props.message;
-  }
   render() {
     return <div>
       <h1>{this.title}</h1>
-     <p>{this.message}</p>
+      <p>{this.message}</p>
     </div>;
   }
 }
