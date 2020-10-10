80b92d96688ecc2d9cec0fd6a4ef44efd7ff8b9e
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-10-10 23:38:40 +0900

    Update Rect.js

--- src/Rect.js
+++ src/Rect.js
@@ -1,17 +1,5 @@
 import React, { Component } from 'react';
 
-
-class App extends Component {
-  render() {
-    return <div>
-  <h1>React</h1>
-      <Rect x="50" y="50" w="150" h="150" c="cyan" />
-      <Rect x="150" y="100" w="150" h="150" c="magenta" />
-      <Rect x="100" y="150" w="150" h="150" c="black" />
-    </div>;
-  }
-}
-
 class Rect extends Component {
   x = 0;
   y = 0;
