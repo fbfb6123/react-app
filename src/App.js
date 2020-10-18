e19b5d6926be3852f5ec10e7162fb060cf51ea91
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-10-18 23:07:24 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,6 +16,36 @@ class App extends Component {
     super(props);
   }
 
-  
+  render() {
+    return (
+      <div>
+        <h1>Redux</h1>
+        <Message />
+        <Button />
+      </div>
+    );
+  }
 }
+
+//ストアのコネクト
+App = connect() (App);
+
+//メッセージの表示コンポーネント
+class Message extends Component {
+  style = {
+    fontSize:"20pt",
+    padding:"20px 5px"
+  }
+
+  render(){
+    return (
+      <p style={this.style}>
+        {this.props.message}:{this.props.counter}
+      </p>
+    );
+  }
+}
+
+//ストアのコネクト
+Message = connect(mappingState) (Message);
 export default App;
