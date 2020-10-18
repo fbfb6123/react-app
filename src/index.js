8ede8e0f747aa965814a79737f6147e5599ea46c
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-10-18 22:50:55 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,32 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// ステートの値
+let state_value = {
+  counter:0,
+  message:"COUNTER"
+}
+
+//レデューサー
+function counter(state = state_value, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        counter:state.counter + 1,
+        message:"INCREMENT"
+      };
+      case 'DECREMENT':
+        return {
+          counter:state.counter - 1,
+          message:"DECREMENT"
+        };
+        default:
+          return state;
+  }
+}
+
+
+
 ReactDOM.render(
     <App msg="Hello App." />,
   document.getElementById('root')
