ccf426ba15a2c50b65e4d781db22f323ba19a6e9
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-11-11 20:45:23 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,38 +4,19 @@ import './App.css';
 import Sampledata from './fire/SampleData';
 import firebase from "firebase";
 
-//ステートのマッピング
-function mappingState(state) {
-  return state;
-}
+// Firebaseの設定
+var firebaseConfig = {
+  apiKey: "AIzaSyB3oCAV7BlVlWPrTD_wFf13jyoVt6sDyvY",
+  authDomain: "react-f5f3c.firebaseapp.com",
+  databaseURL: "https://react-f5f3c.firebaseio.com",
+  projectId: "react-f5f3c",
+  storageBucket: "react-f5f3c.appspot.com",
+  messagingSenderId: "851334510825",
+  appId: "1:851334510825:web:6e28f9153216d3be41d878",
+  measurementId: "G-9VSHFMFXGZ"
+};
 
-//Appのコンポーネント
-class App extends Component {
 
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Redux</h1>
-        <Message />
-        <Button />
-      </div>
-    );
-  }
-}
-
-//ストアのコネクト
-App = connect() (App);
-
-//メッセージの表示コンポーネント
-class Message extends Component {
-  style = {
-    fontSize:"20pt",
-    padding:"20px 5px"
-  }
 
   render(){
     return (
@@ -45,41 +26,4 @@ class Message extends Component {
     );
   }
 }
-
-//ストアのコネクト
-//mappingStateでMessegeのコンポーネントで利用するステート(これで上のpropsの中に値が組み込まれている)
-Message = connect(mappingState) (Message);
-
-//ボタン表示のコンポーネント
-class Button extends Component {
-  style = {
-    fontSize:"16pt",
-    padding:"5px 10px"
-  }
-
-  constructor(props){
-    super(props);
-    this.doAction = this.doAction.bind(this);
-  }
-
-  //ボタンクリックでディスパッチを実行
-  doAction(e) {
-    if (e.shiftKey) {
-      this.props.dispatch({ type:'DECREMENT' });
-    } else {
-      this.props.dispatch ({ type:'INCREMENT' });
-    }
-  }
-
-  render() {
-    return (
-      <button style={this.style}
-      onClick={this.doAction}>Click</button>
-    );
-  }
-}
-
-//ストアのコネクト
-Button = connect()(Button);
-
 export default App;
