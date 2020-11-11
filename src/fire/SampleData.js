2c86f725950ef671e26b57d67e51d9bfdef5b322
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-11-11 20:56:25 +0900

    Update SampleData.js

--- src/fire/SampleData.js
+++ src/fire/SampleData.js
@@ -1,3 +1,20 @@
 import React, {Component} from 'react'
 import firebase from "firebase";
 import "firebase/storage";
+
+class Sampledata extends Component {
+  style = {
+    fontSize:"12pt",
+    padding:"5px 10px"
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data:[]
+    }
+    this.getFireData();
+  }
+
+  
+}
